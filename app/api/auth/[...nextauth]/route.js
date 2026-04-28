01775212cac451babbb3caddff017c63558bc9a9
Nilkanth Patel <nilkanthpat23@gmail.com>
2026-04-27 20:32:56 -0400

    Initial setup - Next.js app with Spotify auth

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth"
+import SpotifyProvider from "next-auth/providers/spotify"
+
+const handler = NextAuth({
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "user-top-read user-read-recently-played user-read-email",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken
+      return session
+    },
+  },
+})
+
+export { handler as GET, handler as POST }
