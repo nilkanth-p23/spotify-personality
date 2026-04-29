8434275dc6d12c6e75095cf9eac9b026b80e4116
Nilkanth Patel <nilkanthpat23@gmail.com>
2026-04-28 20:36:55 -0400

    Added title for app

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,9 @@ import "./globals.css"
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Spotify Personality</title>
+      </head>
       <body>
         <SessionProvider>
           {children}
